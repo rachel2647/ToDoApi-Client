2811bc68619b89b3a554c87e39dc38402362d6c7
rachel2647 <r0548572647@gmail.com>
2023-05-15 22:15:16 +0300

    last

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance=axios.create({baseURL:process.env.REACT_APP_HOST})
+const instance=axios.create({baseURL:process.env.REACT_APP_API_KEY})
 
 instance.interceptors.response.use(
   response => response,
