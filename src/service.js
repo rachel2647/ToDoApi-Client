136af1a888702643a1a86d3af3363ba3e0d64a8f
rachel2647 <r0548572647@gmail>
2023-05-16 23:32:33 +0300

    env-commit

--- src/service.js
+++ src/service.js
@@ -1,34 +1,37 @@
 import axios from 'axios';
 
-
-const instance = axios.create({ baseURL: process.env.RAECT_APP_API })
-
-console.log('process.env.RAECT_APP_API_KEY', process.env.RAECT_APP_API)
-
-instance.interceptors.response.use(
-  response => response,
-  error => {
-    console.error(error);
-  }
-);
+const apiClient=axios.create({
+  baseURL:process.env.REACT_APP_API_KEY
+})
+console.log('process.env.API_URL', process.env.REACT_APP_API_KEY)
 
 export default {
   getTasks: async () => {
-    const result = await instance.get(`/items`);
+    const result = await apiClient.get(`/items`)    
     return result.data;
   },
 
-  addTask: async (name) => {
-    const result = await instance.post(`/items`, { name: name, isComplete: false });
+  addTask: async(name)=>{
+    const result = await apiClient.post(`/items`,{name:name,isComplete:false})    
     return result.data;
   },
 
   setCompleted: async (id, isComplete) => {
-    const result = await instance.put(`/items/${id}?isComplete=${isComplete}`);
-    return result.data;
+    await apiClient.put(`/items/${id}?isComplete=${isComplete}`)
   },
-  deleteTask: async (id) => {
-    const result = await instance.delete(`/items/${id}`);
+
+  deleteTask:async(id)=>{
+    const result = await apiClient.delete(`/items/${id}`)    
     return result.data;
   }
 };
+
+apiClient.interceptors.response.use(function (response) {
+  
+  return response;
+}, function (error) {
+  console.error(error);
+  return Promise.reject(error);
+});
+
+
